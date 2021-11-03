64e89e6043d9278ae272664bf5eba64a7f04149e
Yann Braga <yannbf@gmail.com>
2021-11-03 19:14:56 +0100

    add ts build command

--- lib/configs/csf-strict.ts
+++ lib/configs/csf-strict.ts
@@ -4,7 +4,6 @@
  * in order to update it's content execute "yarn update-all"
  */
 export default {
-  // @ts-expect-error ts-migrate(2580) FIXME: Cannot find name 'require'. Do you need to install... Remove this comment to see the full error message
   extends: require.resolve('./csf'),
   rules: {
     'import/no-anonymous-default-export': 'off',
