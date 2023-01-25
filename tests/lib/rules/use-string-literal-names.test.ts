d2a98f9cfc6ffe2f43a707fc5fab796129c3c39d
Yann Braga <yannbf@gmail.com>
2023-01-25 08:58:50 +0100

    add more test cases

--- tests/lib/rules/use-string-literal-names.test.ts
+++ tests/lib/rules/use-string-literal-names.test.ts
@@ -24,6 +24,11 @@ ruleTester.run('use-string-literal-names', rule, {
     'const A = { name: "N" }; export { A }',
     "const A = { name: 'N' }; export { A }",
     'const A = { name }', // Not a Story (not exported)
+    'const name = String(1994); export const A = { args: { name } }',
+    'export const A = { args: { name: String(1994) } }',
+    'export const A = { play: async () => { const name = String(1994) } }',
+    'export const A = () => {}; A.name = "N"',
+    'export const A = () => {}; A.storyName = "N"',
   ],
   invalid: [
     { code: 'export const A = { name: "1994" + "definitely not my credit card PIN" }', errors },
@@ -33,5 +38,13 @@ ruleTester.run('use-string-literal-names', rule, {
     { code: 'const A = { name: `N` }; export { A }', errors },
     { code: 'const A = { name: String(1994) }; export { A }', errors },
     { code: 'const name = "N"; const A = { name }; export { A }', errors },
+    { code: 'export const A = { storyName: String(1994) };', errors },
+    { code: 'export const A = () => {}; A.name = String(1994)', errors },
+    { code: 'export const A = () => {}; A.storyName = String(1994)', errors },
+    // Not sure how often this actually happens. If too complex, don't take care of it and delete this test
+    {
+      code: 'const ABase = { name: String(1994) }; export const A = { ...ABase, parameters: {} }',
+      errors,
+    },
   ],
 })
